72d1b91ad3ed8a02b1eabdc92d40d67a0151b2d0
Thomas Deconinck <thomas@laclic.fr>
2017-03-25 10:48:15 +0100

    Remove useless file

--- Project 03 - TopMovies/src/utils/router.js
+++
@@ -1,8 +0,0 @@
-import { createRouter } from '@expo/ex-navigation'
-import Home from '../containers/Home'
-import Details from '../containers/Details'
-
-export default createRouter(() => ({
-  home: () => Home,
-  details: () => Details
-}))
